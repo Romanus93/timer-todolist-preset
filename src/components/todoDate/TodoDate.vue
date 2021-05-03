977e15fe5f6c29acc2f2310f03513171c36db317
romanus93 <obj.romanus@gmail.com>
2021-05-03 16:18:34 +0900

    attrs와 focusDate 사용

--- src/components/todoDate/TodoDate.vue
+++ src/components/todoDate/TodoDate.vue
@@ -21,7 +21,7 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   props: {
     date: {
-      type: Date
+      type: Date as any
     },
   },
   data() {
@@ -35,55 +35,23 @@ export default defineComponent({
       ]
     }
   },
-  computed: {
-    calendarComputed: {
-      get(): any {
-        return this.todoDate
-      },
-      set(newValue: any): any {
-        (newValue == null)&&(this.todoDate = this.date)
-      }
-    }
-    // caseNull() {
-    //   console.log('caseNull', this.todoDate);
-    //   (this.todoDate == null)&&(this.todoDate=this.date);
-    //   console.log('caseNull', this.todoDate);
-    // }
-  },
+  // computed: {
+  //   calendarComputed: {
+  //     get(): any {
+  //       return this.todoDate
+  //     },
+  //     set(newValue: any): any {
+  //       (newValue == null)&&(this.todoDate = this.date)
+  //     }
+  //   }
+  // },
   methods: {
-    async focusDatey(date:any): Promise<void> {
+    focusDatey(): void {
       console.log('focusDate');
-      console.log(typeof date);
-      console.log(this.date);
-    //   calendar(): any {
-    //   // const calendars = this.$refs.calendar as object;
-    //   return this.$refs.calendar;
-    // }
       const calendar: any = this.$refs.calendar;
-      console.log(calendar);
-      await calendar.focusDate(this.date);
-      // await calendar.move(this.date);
+      calendar.focusDate(this.date);
       console.log('a');
     }
-  },
-  beforeCreate () {
-    console.log('TodoDate-beforeCreated');
-    console.log(this.date);
-  },
-  created () {
-    console.log('TodoDate-created');
-    console.log(this.date);
-    console.log(this.todoDate);
-  },
-  beforeMount () {
-    console.log('TodoDate-beforeMount');
-    console.log(this.date);
-    console.log(this.todoDate);
-  },
-  mounted() {
-    console.log('TodoDate-mounted');
-    console.log(this.date);
-    console.log(this.todoDate);
   },
   beforeUpdate() {
     console.log('TodoDate-beforeUpdate');
@@ -91,30 +59,20 @@ export default defineComponent({
     console.log('this todoDATE',this.todoDate);
     console.log('attrs[0]',this.attrs[0].dates);
     if(this.todoDate == null){
-      this.todoDate = this.date, this.attrs[0].dates=this.todoDate,console.log('!?!')
+      // this.todoDate = this.date;
+      this.attrs[0].dates=this.date;
+      this.focusDatey();
+      console.log('!?!')
     } else {
-      this.attrs[0].dates= null,console.log('?')
+      this.attrs[0].dates= null;
+        console.log('?');
     }
-    // (this.todoDate == null)&&(this.todoDate = this.date, this.attrs[0].dates=this.todoDate,console.log('!?!'));
-    // (this.todoDate != null)&&(this.attrs[0].dates= null,console.log('?'));
-    console.log('this todoDATE',this.todoDate);
-    console.log('attrs[0]',this.attrs[0].dates);
-    // console.error(this.attrs[0].dates);
-    // if(this.todoDate == null){
-    //     console.error('error-null인경우');
-    //     this.todoDate=this.date
-    //     this.attrs[0].dates = this.todoDate;
-    //     console.error(this.attrs[0].dates);
-    //     // return this.todoDate;
-    //   } else {
-    //     console.error('error-null 아닌 경우');
-    //     this.attrs[0].dates = null;
-    //     console.error(this.attrs[0].dates);
-    //   }
   },
   updated() {
     console.log('TodoDate-updated');
     console.log(this.date);
+    console.log(this.todoDate);
+    
   },
 })
 </script>
