75f63cd69f82a3e4ecf1a6c43ac151baedca7aa1
romanus93 <obj.romanus@gmail.com>
2021-05-25 04:50:25 +0900

    readme 작성, src/.../todoList/...중 todoList-> todolist로 변경

--- src/components/todo-form/TodoForm.vue
+++ src/components/todo-form/TodoForm.vue
@@ -3,14 +3,28 @@
     <ul class="todo-flex todo-info todo-info--bg-image">
       <li class="todo-date">
         <span> {{ date }} </span>
-        <button class="change-date-button button--pocket-calendar" type="button" @click="showPocketCalendar(true)" v-show="!showingPocketCalendar"> <i class="far fa-calendar-alt"></i>  </button>
-        <button class="check-date-button button--pocket-calendar" type="button" @click="showPocketCalendar(false)" v-show="showingPocketCalendar"> <i class="fas fa-calendar-check"></i> </button>
+        <button
+          class="change-date-button button--pocket-calendar"
+          type="button"
+          @click="showPocketCalendar(true)"
+          v-show="!showingPocketCalendar"
+        >
+          <i class="far fa-calendar-alt"></i>
+        </button>
+        <button
+          class="check-date-button button--pocket-calendar"
+          type="button"
+          @click="showPocketCalendar(false)"
+          v-show="showingPocketCalendar"
+        >
+          <i class="fas fa-calendar-check"></i>
+        </button>
         <todo-date
-        :todoDate="date"
-        show="showingPocketCalendar"
-        @checkDate="checkDate"
-        @showPocketCalendar="showPocketCalendar"
-        v-show="showingPocketCalendar"
+          :todoDate="date"
+          show="showingPocketCalendar"
+          @checkDate="checkDate"
+          @showPocketCalendar="showPocketCalendar"
+          v-show="showingPocketCalendar"
         />
       </li>
       <li class="todo-title">
@@ -38,32 +52,58 @@
       <li class="todo-flex todo-time">
         <div class="time-wrap">
           <label for="hours">시간</label>
-          <input type="number" id="hour" class="time-hms" placeholder="0" min="0" v-model.number="hours" oninput="this.value = Math.abs(this.value)" />
+          <input
+            type="number"
+            id="hour"
+            class="time-hms"
+            placeholder="0"
+            min="0"
+            v-model.number="hours"
+            oninput="this.value = Math.abs(this.value)"
+          />
         </div>
-        <div class="time-separator">
-          :
-        </div>
+        <div class="time-separator">:</div>
         <div class="time-wrap">
           <label for="minutes">분</label>
-          <input type="number" id="minutes" class="time-hms" placeholder="0" min="0" v-model.number="minutes" oninput="this.value = Math.abs(this.value)"/>
+          <input
+            type="number"
+            id="minutes"
+            class="time-hms"
+            placeholder="0"
+            min="0"
+            v-model.number="minutes"
+            oninput="this.value = Math.abs(this.value)"
+          />
         </div>
-        <div class="time-separator">
-          :
-        </div>  
+        <div class="time-separator">:</div>
         <div class="time-wrap">
           <label for="seconds">초</label>
-          <input type="number" id="seconds" class="time-hms" placeholder="0" min="0" v-model.number="seconds" oninput="this.value = Math.abs(this.value)" />
+          <input
+            type="number"
+            id="seconds"
+            class="time-hms"
+            placeholder="0"
+            min="0"
+            v-model.number="seconds"
+            oninput="this.value = Math.abs(this.value)"
+          />
         </div>
       </li>
     </ul>
     <ul class="todo-flex create-edit-buttons-wrap">
       <li>
-        <button class="create-edit-button button--calendar button--calendar-image" @click="createUpdateTodo">
+        <button
+          class="create-edit-button button--calendar button--calendar-image"
+          @click="createUpdateTodo"
+        >
           <i class="fas fa-pencil-alt"></i>
         </button>
       </li>
       <li>
-        <button class="home-button button--calendar button--calendar-image" @click="goCalendar()">
+        <button
+          class="home-button button--calendar button--calendar-image"
+          @click="goCalendar()"
+        >
           <i class="fas fa-home"></i>
         </button>
       </li>
@@ -71,17 +111,19 @@
     <div class="check-modal todo-flex" v-show="showingWarning">
       <div class="check-modal__text">
         <p>시간을 입력해주세요.</p>
-        <button @click="showingWarning = false"><i class="fas fa-times"></i> 닫기</button>
+        <button @click="showingWarning = false">
+          <i class="fas fa-times"></i> 닫기
+        </button>
       </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import axios from 'axios'
-import moment from 'moment'
-import { defineComponent } from 'vue'
-import TodoDate from '/src/components/todo-Date/TodoDate.vue'
+import axios from "axios";
+import moment from "moment";
+import { defineComponent } from "vue";
+import TodoDate from "/src/components/todo-date/TodoDate.vue";
 
 interface ParamsTodo {
   date: string;
@@ -93,129 +135,134 @@ interface ParamsTodo {
 }
 
 export default defineComponent({
-  name: 'TodoForm',
+  name: "TodoForm",
   components: {
-    TodoDate
+    TodoDate,
   },
   props: {
     todo: {
-      type: Object as any
-    }
+      type: Object as any,
+    },
   },
-  data () {
+  data() {
     return {
-      date: '',
-      title: '',
-      description: '',
+      date: "",
+      title: "",
+      description: "",
       hours: 0,
       minutes: 0,
       seconds: 0,
       showingPocketCalendar: false,
       showingWarning: false,
-      max: 25
-    }
+      max: 25,
+    };
   },
-  created () {
-    if(this.todo.type === 'create'){
+  created() {
+    if (this.todo.type === "create") {
       this.date = this.todo.date;
     } else {
-      console.log('this.todo.type === edit',this.todo.type);
+      console.log("this.todo.type === edit", this.todo.type);
       this.date = this.todo.date;
       this.title = this.todo.title;
       this.description = this.todo.description;
       this.hours = this.todo.hours;
       this.minutes = this.todo.minutes;
       this.seconds = this.todo.seconds;
-    };
+    }
   },
-  beforeUpdate () {
+  beforeUpdate() {
     this.hrs();
     this.min();
     this.sec();
   },
   methods: {
-    hrs (): void {
-      if(this.hours >= 100) {
+    hrs(): void {
+      if (this.hours >= 100) {
         this.hours = 0;
       } else {
         this.hours = Math.floor(this.hours);
       }
     },
-    min (): void {
-      if(this.minutes >= 60) {
+    min(): void {
+      if (this.minutes >= 60) {
         this.minutes = 0;
       } else {
         this.minutes = Math.floor(this.minutes);
       }
     },
-    sec (): void {
-      if(this.seconds >= 60) {
+    sec(): void {
+      if (this.seconds >= 60) {
         this.seconds = 0;
       } else {
         this.seconds = Math.floor(this.seconds);
       }
     },
-    goCalendar (): void {
-      sessionStorage.removeItem('todo');
+    goCalendar(): void {
+      sessionStorage.removeItem("todo");
       let today: string = moment(new Date()).format("YYYY-MM-DD");
-      (today == this.date )? this.$router.push({name: "Calendar" }) : this.$router.push({name: "Calendar", params: {todoDate: this.date} });
+      today == this.date
+        ? this.$router.push({ name: "Calendar" })
+        : this.$router.push({
+            name: "Calendar",
+            params: { todoDate: this.date },
+          });
     },
-    showPocketCalendar (param: boolean): void {
+    showPocketCalendar(param: boolean): void {
       this.showingPocketCalendar = param;
     },
-    checkDate (changedDate: string): void {
+    checkDate(changedDate: string): void {
       this.date = changedDate;
     },
-    createUpdateTodo () : void {
+    createUpdateTodo(): void {
       let totalTime: number = this.hours + this.minutes + this.seconds;
-      if(totalTime === 0 ){
+      if (totalTime === 0) {
         this.showingWarning = true;
-        return
+        return;
       } else {
         this.postOrPut();
       }
     },
-    postOrPut() : void {
+    postOrPut(): void {
       let params: ParamsTodo = {
         date: this.date,
         title: this.title,
         description: this.description,
         hours: this.hours,
         minutes: this.minutes,
-        seconds: this.seconds
+        seconds: this.seconds,
       };
-      if(this.todo.type === 'edit') {
+      if (this.todo.type === "edit") {
         this.axiosPut(params);
       } else {
         this.axiosPost(params);
       }
     },
-    async axiosPut(params: ParamsTodo ): Promise<void> {
+    async axiosPut(params: ParamsTodo): Promise<void> {
       const res = await axios
-          .put("http://localhost:3005/todolist/"+this.todo.id, params)
-          .then(response => {
-            console.debug(response.data);
-            this.goCalendar();
-          })
-          .catch(error => {
-            console.debug(error);
-          });
+        .put("http://localhost:3005/todolist/" + this.todo.id, params)
+        .then((response) => {
+          console.debug(response.data);
+          this.goCalendar();
+        })
+        .catch((error) => {
+          console.debug(error);
+        });
     },
-    async axiosPost(params: ParamsTodo ): Promise<void> {
+    async axiosPost(params: ParamsTodo): Promise<void> {
       const res = await axios
-          .post("http://localhost:3005/todolist", params)
-          .then(response => {
-            console.debug(response.data);
-            this.goCalendar();
-          })
-          .catch(error => {
-            console.debug(error);
-          });
-    }
-  }
-})
+        .post("http://localhost:3005/todolist", params)
+        .then((response) => {
+          console.debug(response.data);
+          this.goCalendar();
+        })
+        .catch((error) => {
+          console.debug(error);
+        });
+    },
+  },
+});
 </script>
 
 <style scoped>
-@import "./todo-form.css"
+@import "./todo-form.css";
 </style>
--- src/pages/calendar/Calendar.vue
+++ src/pages/calendar/Calendar.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="calendar-page todo-flex">
     <section class="v-calendar-component">
-      <v-date-picker 
+      <v-date-picker
         v-model="date"
         ref="calendar"
         color="blue"
@@ -14,32 +14,46 @@
     </section>
     <section class="todolist-component">
       <div v-show="inputtedDate">
-        <todo-list
-          :inputtedDate="inputtedDate"
-        ></todo-list>
+        <todo-list :inputtedDate="inputtedDate"></todo-list>
       </div>
       <div class="date-none-modal" v-show="!inputtedDate">
         <p class="date-none-modal__text">날짜를 선택해주세요!</p>
       </div>
     </section>
     <section class="todo-flex action-buttons-wrap" v-show="inputtedDate">
-        <div>
-          <button type="button" class="yesterday-button button--calendar button--calendar-image" @click="goYesterday">
-            <i class="fas fa-arrow-left"></i>
-          </button>
-        </div>
-        <div>
-          <button type="button" class="today-button button--calendar button--calendar-image" @click="goToday">
-            <span class="today"> {{ today }} </span>
-          </button>
-        </div>
-        <div>
-          <button type="button" class="tomorrow-button button--calendar button--calendar-image" @click="goTomorrow">
-            <i class="fas fa-arrow-right"></i>
-          </button>
-        </div>
       <div>
-        <button type="button" class="create-button button--calendar button--calendar-image" @click="goCreateTodo">
+        <button
+          type="button"
+          class="yesterday-button button--calendar button--calendar-image"
+          @click="goYesterday"
+        >
+          <i class="fas fa-arrow-left"></i>
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          class="today-button button--calendar button--calendar-image"
+          @click="goToday"
+        >
+          <span class="today"> {{ today }} </span>
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          class="tomorrow-button button--calendar button--calendar-image"
+          @click="goTomorrow"
+        >
+          <i class="fas fa-arrow-right"></i>
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          class="create-button button--calendar button--calendar-image"
+          @click="goCreateTodo"
+        >
           <i class="fas fa-plus"></i>
         </button>
       </div>
@@ -50,80 +64,84 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import moment from "moment";
-import TodoList from "/src/components/todolist/TOdolist.vue"
+import TodoList from "/src/components/todolist/Todolist.vue";
 
 export default defineComponent({
-  name: 'Calendar',
+  name: "Calendar",
   components: { TodoList },
   data() {
     return {
       date: new Date() as object,
       position: 1,
       transition: "slide-v",
-      calendarView: false
+      calendarView: false,
       //'slide-h', 'slide-v', 'fade', 'none' 중 선택 가능. default slide-h
     };
   },
   computed: {
-    inputtedDate (): any {
+    inputtedDate(): any {
       if (this.date != null) {
         return moment(this.date).format("YYYY-MM-DD");
       } else {
         return 0;
       }
     },
-    today (): string {
-      return moment(new Date()).format('DD')
+    today(): string {
+      return moment(new Date()).format("DD");
     },
-    calendar (): any {
+    calendar(): any {
       return this.$refs.calendar;
-    }
+    },
   },
-  created () {
-    (this.$route.params.todoDate !== undefined)&&this.resetDate();
+  created() {
+    this.$route.params.todoDate !== undefined && this.resetDate();
   },
   methods: {
-    goToday (): void {
+    goToday(): void {
       this.date = new Date();
       this.calendar.move(this.date);
     },
-    goYesterday (): void {
-      let startOfMonth : string = moment(this.date).startOf('month').format('YYYY-MM-DD');
+    goYesterday(): void {
+      let startOfMonth: string = moment(this.date)
+        .startOf("month")
+        .format("YYYY-MM-DD");
       let step: number = 0;
-      (this.inputtedDate == startOfMonth ) ? step = -1 : step = 0;
+      this.inputtedDate == startOfMonth ? (step = -1) : (step = 0);
       this.date = moment(this.date).subtract(1, "day").toDate();
       this.goMonth(step);
     },
-    goTomorrow (): void {
-      let endOfMonth : string = moment(this.date).endOf("month").format("YYYY-MM-DD");
+    goTomorrow(): void {
+      let endOfMonth: string = moment(this.date)
+        .endOf("month")
+        .format("YYYY-MM-DD");
       let step: number = 0;
-      (this.inputtedDate == endOfMonth ) ? step = 1 : step = 0;
+      this.inputtedDate == endOfMonth ? (step = 1) : (step = 0);
       this.date = moment(this.date).add(1, "day").toDate();
       this.goMonth(step);
     },
-    goMonth (step: number): void {
+    goMonth(step: number): void {
       // step: -1 이전 달, 0 이동 없음, 1 다음 달
       this.calendar.move(step, {
         transition: this.transition,
-        position: this.position
+        position: this.position,
       });
     },
-    goCreateTodo (): void {
-      let todo:any = {
-        date : this.inputtedDate,
-        path: 'create-todo',
-        type: 'create'
+    goCreateTodo(): void {
+      let todo: any = {
+        date: this.inputtedDate,
+        path: "create-todo",
+        type: "create",
       };
-      this.$router.push({ name: "CreateEditTodo" , params: todo });
+      this.$router.push({ name: "CreateEditTodo", params: todo });
     },
-    resetDate (): void{
+    resetDate(): void {
       let resetDate: any = moment(this.$route.params.todoDate);
       this.date = resetDate._d;
-    }
-  }
+    },
+  },
 });
 </script>
 
 <style scoped>
-@import "./calendar.css"; 
+@import "./calendar.css";
 </style>
